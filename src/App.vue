625c6bd072967f47388857168b08f83f6dd7d0d7
chenlixian <chenlixian@paohe.cn>
2019-10-23 15:00:12 +0800

    [v1.0.0] update基础架构修改一

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <router-view v-if="isRouterAlive"></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './views/Test.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data () {
+    return {
+      isRouterAlive: true
+    }
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/store.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import VueX from 'vuex'
+
+Vue.use(VueX)
