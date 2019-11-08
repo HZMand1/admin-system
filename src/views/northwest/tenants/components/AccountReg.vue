35192051da12f24a07393b1b642aff707465aba3
shenmaorui <1246054720@qq.com>
2019-11-08 10:14:08 +0800

    【v1.0.0】update：编写商家入驻页面

---
+++ src/views/northwest/tenants/components/AccountReg.vue
@@ -0,0 +1,89 @@
+<template>
+  <!-- 商家账号申请 -->
+  <section class="account-reg">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px">
+      <el-row>
+        <el-col :span="13" :offset="4">
+          <el-form-item label="账号 : " prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="13" :offset="4">
+          <el-form-item label="密码 : " prop="password">
+            <el-input v-model="ruleForm.password" type="password"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="13" :offset="4">
+          <el-form-item label="确认密码 : " prop="cPassword">
+            <el-input v-model="ruleForm.cPassword" type="password"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="13" :offset="4">
+          <el-form-item label="联系人 : " prop="contact">
+            <el-input v-model="ruleForm.contact"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="10" :offset="4">
+          <el-form-item label="手机验证码 : " prop="telCode">
+            <el-input v-model="ruleForm.telCode"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="3" class="pad-l-10">
+          <div class="get-code">获取验证码</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :offset="10" :span="14">
+          <el-button class="enter" @click="application('ruleForm')">立即入驻</el-button>
+        </el-col>
+      </el-row>
+    </el-form>
+  </section>
+</template>
+<script>
+export default {
+  name: "AccountReg",
+  data() {
+    return {
+      ruleForm: {},
+      rules: {
+        account: [{ required: true, message: "请输入账号", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+        cPassword: [
+          { required: true, message: "请再次输入密码", trigger: "blur" }
+        ],
+        contact: [{ required: true, message: "请输入联系人", trigger: "blur" }],
+        telCode: [
+          { required: true, message: "请输入手机验证码", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    // 商家入驻触发下一步
+    application(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$emit("nextEmit", 2);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
---
+++ src/views/northwest/tenants/components/AptitudeReg.vue
@@ -0,0 +1,109 @@
+<template>
+  <!-- 资质申请 -->
+  <section>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="180px">
+      <el-row>
+        <el-col :span="13" :offset="4">
+          <el-form-item label="类型 : " prop="type">
+            <el-select v-model="ruleForm.type">
+              <el-option label="个人" value="personal"></el-option>
+              <el-option label="企业" value="enterprise"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row v-if="ruleForm.type==='enterprise'">
+        <el-col :span="13" :offset="4">
+          <el-form-item label="企业名称 : " prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row v-if="ruleForm.type==='enterprise'">
+        <el-col :span="13" :offset="4">
+          <el-form-item label="企业法人 : " prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row v-if="ruleForm.type==='personal'">
+        <el-col :span="13" :offset="4">
+          <el-form-item label="持有人 : " prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row v-if="ruleForm.type==='personal'">
+        <el-col :span="13" :offset="4">
+          <el-form-item label="持有人电话 : " prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="13" :offset="4">
+          <el-form-item :label="ruleForm.type==='personal' ?'持有人身份证 : ':' 法人身份证号 : '" prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="13" :offset="4">
+          <el-form-item :label="ruleForm.type==='personal' ?'身份证照片 : ':' 法人身份证照片 : '" prop="account">
+            <horizontal-upload-image></horizontal-upload-image>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row v-if="ruleForm.type==='personal'">
+        <el-col :span="13" :offset="4">
+          <el-form-item label="自产自销证明 : " prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row v-if="ruleForm.type==='enterprise'">
+        <el-col :span="13" :offset="4">
+          <el-form-item label="企业证件照 : " prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="13" :offset="4">
+          <el-form-item label="银行卡号 : " prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="13" :offset="4">
+          <el-form-item label="开户行 : " prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :offset="10" :span="5">
+          <el-button class="enter" @click="application('ruleForm')">下一步</el-button>
+        </el-col>
+      </el-row>
+    </el-form>
+  </section>
+</template>
+<script>
+import HorizontalUploadImage from "@/components/HorizontalUploadImage";
+export default {
+  name: "AptitudeReg",
+  components: { HorizontalUploadImage },
+  data() {
+    return {
+      ruleForm: {
+        type: "personal"
+      },
+      rules: {}
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
